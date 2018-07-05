2924d309d4c141b343ab9f1f5b0f839c17aa3978
Christopher Milan <chris.m.milan@gmail.com>
2018-07-05 11:48:37 -0400

    Add Arduino code

---
+++ sensor/BluefruitConfig.h
@@ -0,0 +1,4 @@
+#define BUFSIZE                    128   // Size of the read buffer for incoming data
+#define VERBOSE_MODE               true  // If set to 'true' enables debug output
+#define BLUEFRUIT_HWSERIAL_NAME    Serial1
+#define BLUEFRUIT_UART_MODE_PIN    12    // Set to -1 if unused

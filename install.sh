4227e4cd8ab98e22ff4200e2a40f878eb6b7649d
Christopher Milan <chris.m.milan@gmail.com>
2018-10-30 17:55:44 -0400

    Update install script to install to proper locations.
    Signed-off-by: Christopher Milan <chris.m.milan@gmail.com>

--- install.sh
+++ install.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
+echo "Creating system files..."
+mkdir -p /usr/share/thermoPi/dist
+
+
 echo "Compiling Vue.js webapp..."
 cd public
 echo "Installing packages..."
 npm install
-echo "Building Vue project..."
+echo "Building Vue.js project..."
 npm run build
+echo "Moving Vue.js project to /usr/share/thermoPi/dist"
+mv dist/* /usr/share/thermoPi/dist/
 cd ..
 
 echo "Installing Golang backend"
-go install -i
-if [[ -z $GOBIN ]]
-then
-echo "GOBIN unset. Is go installed properly?"
-echo "Nonetheless, thermoPi has been likely installed to GOPATH/bin/${PWD##*/}"
-echo "Run go env to discover your GOPATH."
-else
-echo "thermoPi has been installed to $GOBIN${PWD##*/}"
-fi
+go install -i /usr/local/bin
+echo "thermoPi has been installed to /usr/local/bin"

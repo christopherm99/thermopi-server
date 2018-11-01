7a50fbba797c4b88b7edaca8144656594e78c36e
Christopher Milan <chris.m.milan@gmail.com>
2018-11-01 10:47:59 -0400

    Update install script to include Y/N option for installing Vue.js webapp.
    Signed-off-by: Christopher Milan <chris.m.milan@gmail.com>

--- install.sh
+++ install.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-echo "Creating system files..."
+install_vue() {
+    echo "Cloning git repo..."
+    git clone https://github.com/christopherm99/thermopi-webapp public
+    cd public
+    echo "Installing production dependencies..."
+    npm install --production
+    echo "Building Vue.js project..."
+    npm run build
+    echo "Moving Vue.js project to /usr/share/thermoPi/dist..."
+    mv dist/* /usr/share/thermoPi/dist/
+    cd ..
+}
+
+echo "Creating program files..."
 mkdir -p /usr/share/thermoPi/dist
 
+read -p "Do you wish to install the default webapp, https://github.com/christopherm99/thermopi-webapp [Y/n]?" yn
+case $yn in
+    [Nn]* ) echo "Not installing default webapp. To host files, place them in /usr/share/thermoPi/dist";;
+    * ) install_vue;;
+esac
 
-echo "Compiling Vue.js webapp..."
-cd public
-echo "Installing packages..."
-npm install
-echo "Building Vue.js project..."
-npm run build
-echo "Moving Vue.js project to /usr/share/thermoPi/dist"
-mv dist/* /usr/share/thermoPi/dist/
-cd ..
-
-echo "Installing Golang backend"
+echo "Installing Golang backend..."
 go install -i /usr/local/bin
+
+echo "Setting up program files..."
+cp schedule.csv /usr/share/thermoPi/schedule.csv
+chown -r ${USER} /usr/share/thermoPi/dist
+
 echo "thermoPi has been installed to /usr/local/bin"

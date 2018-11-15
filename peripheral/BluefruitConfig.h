c2f5c065c2bbdf91162194ba17d1a34855c9d588
Christopher Milan <chris.m.milan@gmail.com>
2018-11-14 20:54:22 -0500

    Remove residual bluetooth code.
    Signed-off-by: Christopher Milan <chris.m.milan@gmail.com>

--- peripheral/BluefruitConfig.h
+++
@@ -1,4 +0,0 @@
-#define BUFSIZE                    128   // Size of the read buffer for incoming data
-#define VERBOSE_MODE               true  // If set to 'true' enables debug output
-#define BLUEFRUIT_HWSERIAL_NAME    Serial1
-#define BLUEFRUIT_UART_MODE_PIN    12    // Set to -1 if unused
